648c63292645a2375b3fc8bfb75119e9c76b2c38
TheDimension5 <154542884+TheDimension5@users.noreply.github.com>
2026-09-17 15:34:06 -0400

    Add The Eyepiece: telescope-to-microscope scale viewer, 62 powers of ten, with missions, kits and wiring

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='chalk-v2';const HUB='https://thedimension5.github.io/chalkboard/';const PAGES=["", "boards/", "map/", "classroom/", "einstein/", "launch/", "heat/", "electricity/", "infinity/", "history/", "life/", "kits/"].map(p=>HUB+p);
+const V='chalk-v3';const HUB='https://thedimension5.github.io/chalkboard/';const PAGES=["", "boards/", "map/", "classroom/", "einstein/", "launch/", "heat/", "electricity/", "infinity/", "history/", "life/", "scale/", "kits/"].map(p=>HUB+p);
 self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(PAGES).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(e.request.method!=='GET')return;
